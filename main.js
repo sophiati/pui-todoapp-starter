ecd9d9b35c55321e9f13915107e8f3e13be51861
sophiati <sophiati@andrew.cmu.edu>
2021-10-04 11:28:47 -0400

    Week 6 Lab edits

--- main.js
+++ main.js
@@ -2,3 +2,24 @@ function addNewList() {
   alert("hello world alert!")
   console.log("hello world console")
 }
+
+function addListItem() {
+  console.log("hello world")
+  let list = document.getElementById("grocery-list")
+  let itemInput = document.getElementById("text_input")
+  let newItem = document.createElement("li")
+  newItem.appendChild(document.createTextNode(itemInput.value))
+  list.appendChild(newItem)
+}
+
+function deleteListItem(item) {
+  item.parentNode.removeChild(item)
+}
+
+function completeListItem(item) {
+  if (item.checked) {
+    item.parentNode.style.textDecoration = "line-through"
+  } else {
+    item.parentNode.style.textDecoration = "none"
+  }
+}
